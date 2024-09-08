59dfd29fb25316a711ef7db0a25734f69f68f01e
ansariaalima610@gmail.com <ansariaalima510@gmail.com>
2024-09-08 09:27:50 -0700

    Add new Code

--- index.php
+++ index.php
@@ -1,2 +1,49 @@
-<p>Registration Page</p>
- <h4>Connect with database.</h4>
+<h1>Registration Form</h1>
+<style>
+      h1{
+      /* color:red; */
+      text-align: center;
+      /* background-color: orange; */
+     font-size: 45px;
+     
+
+   }
+</style>
+
+<?php
+if(isset($_POST['submit'])){
+
+$name= $_POST['name'];
+$email= $_POST['email'];
+$password= $_POST['password'];
+$query= "INSERT into user (name, email, password) VALUES ('$name','$email','$password')";
+echo $query;
+$run= mysqli_query($conn,$query);
+if($run){
+  echo "<script>";
+  echo 'alert("Data Inserted")';
+  echo "</script>";
+  header("Location:login.php");
+}else{
+  echo "Something went wrong";
+}
+}else{
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Calculator</title>
+</head>
+<body>
+<form action= "" method= "POST">
+<label>Enter your Name:</label>
+<input type="name" name="name" value=""><br><br>
+<label>Enter your Email:</label>
+<input type="email" name="email" value=""><br><br>
+<label>Enter your Password:</label>
+<input type="password" name="password" value=""><br><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+<?php }?>
